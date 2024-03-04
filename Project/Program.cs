f55ef370d4cc2b3392e2024ca9c0bdb147d67777
Pauls <pauls2odins@gmail.com>
2024-03-04 12:38:04 +0100

    Welcome message

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GameOfLife
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Game of Life!\n");
+            Console.WriteLine("Choose a grid setup:\n1 - New randomized grid\n2 - Load grid from a file");
+        }
+    }
+}
